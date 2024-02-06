fcd5ff5f36d4a710d949aad3c4f0371edf45b5be
OctiGit <octi.aicardi@gmail.com>
2024-02-06 15:09:53 +0100

    change removeUser.js for firebase

--- src/store/thunks/removeUser.js
+++ src/store/thunks/removeUser.js
@@ -1,8 +1,20 @@
+import { collection, deleteDoc, doc, getDocs } from "@firebase/firestore";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+// import axios from "axios";
+import db from "../../Firebase/config";
 
 const removeUser = createAsyncThunk("users/remove", async (user) => {
-  await axios.delete(`http://localhost:3005/users/${user.id}`);
+  // await axios.delete(`http://localhost:3005/users/${user.id}`);
+  // await axios.delete(
+  //   `https://my-json-server.typicode.com/OctiGit/Media/users/${user.id}`
+  // );
+
+  const users = await getDocs(collection(db, "Users"));
+  for (const snap of users.docs) {
+    if (snap.id === user.id) {
+      await deleteDoc(doc(db, "Users", snap.id));
+    }
+  }
 
   // FIX !!!!
   return user;
