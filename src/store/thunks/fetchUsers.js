88347e453c206745d4587fcafd475bdcdf865b00
OctiGit <octi.aicardi@gmail.com>
2024-02-06 15:08:56 +0100

    change  fetchUsers.js for firebase

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -1,13 +1,25 @@
+import { collection, getDocs } from "@firebase/firestore";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+// import axios from "axios";
+import db from "../../Firebase/config";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
-  const response = await axios.get("http://localhost:3005/users");
+  // const response = await axios.get("http://localhost:3005/users");
+  // const response = await axios.get(
+  //   "https://my-json-server.typicode.com/OctiGit/Media/users"
+  // );
+
+  const querySnapshot = await getDocs(collection(db, "Users"));
+  const users = querySnapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
 
   // DEV ONLY!!!
   await pause(1000);
 
-  return response.data;
+  // return response.data;
+  return users;
 });
 
 // DEV ONLY!!!
