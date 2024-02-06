23573c4eced66ee340691b03d3e70f39d4c30b97
OctiGit <octi.aicardi@gmail.com>
2024-02-06 15:08:02 +0100

    change addUser.js for firebase

--- src/store/thunks/addUser.js
+++ src/store/thunks/addUser.js
@@ -1,13 +1,23 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+// import axios from "axios";
 import { faker } from "@faker-js/faker";
+import { addDoc, collection } from "@firebase/firestore";
+import db from "../../Firebase/config";
 
 const addUser = createAsyncThunk("users/add", async () => {
-  const response = await axios.post("http://localhost:3005/users", {
-    name: faker.person.fullName(),
-  });
+  // const response = await axios.post("http://localhost:3005/users", {
+  // const response = await axios.post(
+  //   "https://my-json-server.typicode.com/OctiGit/Media/users",
+  //   {
+  //     name: faker.person.fullName(),
+  //   }
+  // );
+  const user = { name: faker.person.fullName() };
+  const addUserRef = await addDoc(collection(db, "Users"), user);
+  const newUser = { ...user, id: addUserRef.id };
 
-  return response.data;
+  // return response.data;
+  return newUser;
 });
 
 export { addUser };
