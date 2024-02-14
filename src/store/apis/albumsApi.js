f45b14805def33d57910ad9b6ca8dcee0670afce
OctiGit <octi.aicardi@gmail.com>
2024-02-14 16:15:51 +0100

    use fakeBaseQuery and queryFn for endpoint calls to Firestore

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,50 +1,60 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  createApi,
+  fakeBaseQuery,
+  // fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 import { faker } from "@faker-js/faker";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+} from "@firebase/firestore";
+import { db } from "../../Firebase/config";
 
 // DEV ONLY!!!
-const pause = (duration) => {
-  return new Promise((resolve) => setTimeout(resolve, duration));
-};
+// const pause = (duration) => {
+//   return new Promise((resolve) => setTimeout(resolve, duration));
+// };
 
 const albumsApi = createApi({
   reducerPath: "albums",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3005",
-    fetchFn: async (...args) => {
-      // REMOVE FOR PRODUCTION
-      await pause(1000);
-      return fetch(...args);
-    },
-  }),
+  // baseQuery: fetchBaseQuery({
+  //   baseUrl: "http://localhost:3005",
+  //   fetchFn: async (...args) => {
+  //     // REMOVE FOR PRODUCTION
+  //     await pause(1000);
+  //     return fetch(...args);
+  //   },
+  // }),
+  baseQuery: fakeBaseQuery(),
   endpoints(builder) {
     return {
-      removeAlbum: builder.mutation({
-        invalidatesTags: (result, error, album) => {
-          return [{ type: "Album", id: album.id }];
-        },
-        query: (album) => {
-          return {
-            url: `albums/${album.id}`,
-            method: "DELETE",
-          };
-        },
-      }),
-      addAlbum: builder.mutation({
-        invalidatesTags: (result, error, user) => {
-          return [{ type: "UsersAlbums", id: user.id }];
-        },
-        query: (user) => {
-          return {
-            url: "/albums",
-            method: "POST",
-            body: {
-              userId: user.id,
-              title: faker.commerce.productName(),
-            },
-          };
-        },
-      }),
       fetchAlbums: builder.query({
+        // query: (user) => {
+        //   return {
+        //     url: "/albums",
+        //     params: {
+        //       userId: user.id,
+        //     },
+        //     method: "GET",
+        //   };
+        // },
+        async queryFn(user) {
+          try {
+            const querySnapshot = await getDocs(collection(db, "Albums"));
+            const albums = querySnapshot.docs
+              .map((doc) => ({
+                ...doc.data(),
+                id: doc.id,
+              }))
+              .filter((album) => album.userId === user.id);
+            return { data: albums };
+          } catch (err) {
+            return { error: err };
+          }
+        },
         providesTags: (result, error, user) => {
           const tags = result.map((album) => {
             return { type: "Album", id: album.id };
@@ -52,14 +62,51 @@ const albumsApi = createApi({
           tags.push({ type: "UsersAlbums", id: user.id });
           return tags;
         },
-        query: (user) => {
-          return {
-            url: "/albums",
-            params: {
+      }),
+      addAlbum: builder.mutation({
+        // query: (user) => {
+        //   return {
+        //     url: "/albums",
+        //     method: "POST",
+        //     body: {
+        //       userId: user.id,
+        //       title: faker.commerce.productName(),
+        //     },
+        //   };
+        // },
+        async queryFn(user) {
+          try {
+            const album = {
               userId: user.id,
-            },
-            method: "GET",
-          };
+              title: faker.commerce.productName(),
+            };
+            const albumRef = await addDoc(collection(db, "Albums"), album);
+            return { ...album, id: albumRef.id };
+          } catch (err) {
+            return { error: err };
+          }
+        },
+        invalidatesTags: (result, error, user) => {
+          return [{ type: "UsersAlbums", id: user.id }];
+        },
+      }),
+      removeAlbum: builder.mutation({
+        // query: (album) => {
+        //   return {
+        //     url: `albums/${album.id}`,
+        //     method: "DELETE",
+        //   };
+        // },
+        async queryFn(album) {
+          try {
+            await deleteDoc(doc(db, "Albums", album.id));
+            return { data: "album removed successfully" };
+          } catch (err) {
+            return { error: err };
+          }
+        },
+        invalidatesTags: (result, error, album) => {
+          return [{ type: "Album", id: album.id }];
         },
       }),
     };
--- src/store/apis/photosApi.js
+++ src/store/apis/photosApi.js
@@ -1,5 +1,13 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { faker } from "@faker-js/faker";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+} from "@firebase/firestore";
+import { db } from "../../Firebase/config";
 
 const photosApi = createApi({
   reducerPath: "photos",
@@ -9,6 +17,29 @@ const photosApi = createApi({
   endpoints(builder) {
     return {
       fetchPhotos: builder.query({
+        // query: (album) => {
+        //   return {
+        //     method: "GET",
+        //     url: "/photos",
+        //     params: {
+        //       albumId: album.id,
+        //     },
+        //   };
+        // },
+        async queryFn(album) {
+          try {
+            const querySnapshot = await getDocs(collection(db, "Photos"));
+            const photos = querySnapshot.docs
+              .map((doc) => ({
+                ...doc.data(),
+                id: doc.id,
+              }))
+              .filter((photo) => photo.albumId === album.id);
+            return { data: photos };
+          } catch (err) {
+            return { error: err };
+          }
+        },
         providesTags: (result, error, album) => {
           const tags = result.map((photo) => {
             return { type: "Photo", id: photo.id };
@@ -16,40 +47,51 @@ const photosApi = createApi({
           tags.push({ type: "AlbumPhoto", id: album.id });
           return tags;
         },
-        query: (album) => {
-          return {
-            method: "GET",
-            url: "/photos",
-            params: {
-              albumId: album.id,
-            },
-          };
-        },
       }),
       addPhoto: builder.mutation({
+        // query: (album) => {
+        //   return {
+        //     method: "POST",
+        //     url: "/photos",
+        //     body: {
+        //       url: faker.image.abstract(150, 150, true),
+        //       albumId: album.id,
+        //     },
+        //   };
+        // },
+        async queryFn(album) {
+          try {
+            const photo = {
+              url: faker.image.abstract(150, 150, true),
+              albumId: album.id,
+            };
+            const photoRef = await addDoc(collection(db, "Photos"), photo);
+            return { ...photo, id: photoRef.id };
+          } catch (err) {
+            return { error: err };
+          }
+        },
         invalidatesTags: (result, error, album) => {
           return [{ type: "AlbumPhoto", id: album.id }];
         },
-        query: (album) => {
-          return {
-            method: "POST",
-            url: "/photos",
-            body: {
-              url: faker.image.abstract(150, 150, true),
-              albumId: album.id,
-            },
-          };
-        },
       }),
       removePhoto: builder.mutation({
+        // query: (photo) => {
+        //   return {
+        //     method: "DELETE",
+        //     url: `/photos/${photo.id}`,
+        //   };
+        // },
+        async queryFn(photo) {
+          try {
+            await deleteDoc(doc(db, "Photos", photo.id));
+            return { data: "photo removed successfully" };
+          } catch (err) {
+            return { error: err };
+          }
+        },
         invalidatesTags: (result, error, photo) => {
           return [{ type: "Photo", id: photo.id }];
-        },
-        query: (photo) => {
-          return {
-            method: "DELETE",
-            url: `/photos/${photo.id}`,
-          };
         },
       }),
     };
